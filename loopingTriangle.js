65e35941f9a072a0581c686d0552c997826e8f7a
Jamin Ewell <jamin.ewell@yahoo.com>
2020-10-11 18:11:20 -0700

    looping triangle
    created triangle function

---
+++ loopingTriangle.js
@@ -0,0 +1,24 @@
+/*
+Write a loop that displays the following triangle within a console window:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+function printTriangle() {
+
+    var count = 7;
+    var triangle = '';
+
+    for (var i = 0; i < count; i++) {
+        triangle += '#';
+        console.log(triangle);
+    }
+
+
+
+}
